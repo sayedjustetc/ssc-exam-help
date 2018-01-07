4a66cc28fe389196758f14e1bfa7e16fea6ef875
sayed ahmed <sayedum@gmail.com>
2018-01-06 20:06:08 -0500

    cleanup and initial file add

---
+++ app/src/main/java/com/justetc/oral_communications/workplace/MyWebViewClient.java
@@ -0,0 +1,12 @@
+package com.justetc.oral_communications.workplace;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+}
